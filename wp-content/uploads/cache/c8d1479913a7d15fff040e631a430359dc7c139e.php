9e709d1122aaf690bed1d39dedfcaf3d8cc81d66
haminhtai <haminhtai.w2@gmail.com>
2022-01-01 16:58:35 +0700

    tai

--- wp-content/uploads/cache/c8d1479913a7d15fff040e631a430359dc7c139e.php
+++ wp-content/uploads/cache/c8d1479913a7d15fff040e631a430359dc7c139e.php
@@ -1,9 +1,11 @@
 <section class="mod-about_us px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
     <div class="container">
         <div class="max-w-[640px] sm:text-center sm:mx-auto fadeInDown visible relative z-2">
-            {!! $data->module['content']!!}
-            <img src="{!! $data->module['image']->url!!}" alt="{!! $data->module['image']->alt!!}" class="w-14 text-center mx-auto mb-2"/>
-            {!! $data->module['sub_content']!!}
+            <?php echo $data->module['content']; ?>
+
+            <img src="<?php echo $data->module['image']->url; ?>" alt="<?php echo $data->module['image']->alt; ?>" class="w-14 text-center mx-auto mb-2"/>
+            <?php echo $data->module['sub_content']; ?>
+
             <hr class="w-full my-8 border-gray-300" />
         </div>
     </div>
